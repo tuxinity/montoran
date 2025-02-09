4b3e17c2e3a787dfff349eebc1bc522849c34246
Y0SH4 <yosa.agung13@gmail.com>
2025-02-09 16:51:58 +0700

    add dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
